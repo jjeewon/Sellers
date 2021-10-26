d65358f5e8a607fe88730318500ab326955b0691
jjeewon <wldnjs594@gmail.com>
2021-10-26 18:18:51 +0900

    회원가입 정보 validate 함수 정의

--- api/src/main/kotlin/com/example/sellers/domain/auth/SignupService.kt
+++ api/src/main/kotlin/com/example/sellers/domain/auth/SignupService.kt
@@ -1,7 +1,31 @@
 package com.example.sellers.domain.auth
 
+import com.example.sellers.SellersException
 import com.example.sellers.domain.user.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 
 class SignupService @Autowired constructor(private val userRepository: UserRepository) {
+
+    private fun validateRequest(signUpRequest: SignUpRequest){
+        validateEmail(signUpRequest.email)
+        validateName(signUpRequest.name)
+        validatePassword(signUpRequest.password)
+    }
+    private fun validateEmail(email: String){
+        val isNotValidEmail = "^[A-Z0-9._%+]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$"
+            .toRegex(RegexOption.IGNORE_CASE)
+            .matches(email)
+            .not()
+        if(isNotValidEmail){
+            throw SellersException("이메일 형식이 올바르지 않습니다.")
+        }
+    }
+    private fun validateName(name: String){
+        if (name.trim().length !in 2..20)
+            throw SellersException("이름은 2자 이상 20자 이하여야 합니다.")
+    }
+    private fun validatePassword(password: String){
+        if(password.trim().length !in 8..20)
+            throw SellersException("비밀번호는 공백을 제외하고 8자 이상 20자 이하여야 합니다.")
+    }
 }
