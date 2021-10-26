684dbf55fac18bc92e9bafb78cda65348e761cb2
jjeewon <wldnjs594@gmail.com>
2021-10-26 20:52:34 +0900

    ApiResponse 데이터 클래스 정의

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/api/response/ApiResponse.kt
@@ -0,0 +1,11 @@
+package com.gomdolstudio.sellers.api.response
+
+data class ApiResponse<T> (
+    val success: Boolean,
+    val data: T? = null,
+    val message: String? = null
+){
+    companion object{
+        inline fun <reified T> error(message: String? = null) = ApiResponse(false, null as T?, message)
+    }
+}
