5f85a96bb74174d24ce8e0b94d3adbbf6c0b8711
jjeewon <wldnjs594@gmail.com>
2021-10-27 21:34:09 +0900

    회원가입 뷰모델 정의

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/SignupViewModel.kt
@@ -0,0 +1,79 @@
+package com.gomdolstudio.sellers.ui
+
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.gomdolstudio.covidinfoapp.di.factory.AssistedSavedStateViewModelFactory
+import com.gomdolstudio.sellers.api.request.SignupRequest
+import com.gomdolstudio.sellers.api.response.ApiResponse
+import com.gomdolstudio.sellers.data.service.HelloService
+import com.gomdolstudio.sellers.data.service.SignupService
+import com.gomdolstudio.sellers.util.SingleLiveEvent
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.time.LocalDate
+
+class SignupViewModel @AssistedInject constructor(@Assisted private val savedStateHandle: SavedStateHandle,
+                                                private val signupService: SignupService): ViewModel(){
+    private val compositeDisposable = CompositeDisposable()
+
+    val inputEmail: MutableLiveData<String> = MutableLiveData("")
+    val inputName: MutableLiveData<String> = MutableLiveData("")
+    val inputPassword: MutableLiveData<String> = MutableLiveData("")
+
+    private val showToast = SingleLiveEvent<String>()
+    fun getShowToast(): SingleLiveEvent<String>{
+        return showToast
+    }
+
+    fun signup(){
+        val request = SignupRequest(inputEmail.value!!,inputPassword.value!!,inputName.value!!)
+        if (isNotValidSignup(request)) return
+        requestSignup(request)
+    }
+    private fun isNotValidSignup(signupRequest: SignupRequest) =
+        when {
+            signupRequest.isNotValidEmail() -> {
+                showToast.postValue( "이메일 형식이 정확하 않습니다.")
+                true
+            }
+            signupRequest.isNotValidName() -> {
+                showToast.postValue( "이름은 2자 이상 20자 이하로 입력해주세요.")
+                true
+            }
+            signupRequest.isNotValidPassword() -> {
+                showToast.postValue( "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+                true
+            }
+            else -> false
+        }
+    private fun requestSignup(request: SignupRequest){
+        compositeDisposable.add(signupService.signup(request)
+            .subscribeOn(Schedulers.io())
+            .subscribe(::onSignupResponse))
+    }
+    private fun onSignupResponse(response: ApiResponse<Void>) {
+        if (response.success) {
+            showToast.postValue( "회원 가입이 되었습니다. 로그인 후 이용해주세요.")
+        } else {
+            showToast.postValue( response.message ?: "알 수 없는 오류가 발생했습니다.")
+        }
+    }
+
+
+    fun loadName(): String{
+        val email = inputEmail.value!!
+        val name = inputName.value!!
+        val password = inputPassword.value!!
+
+        return inputName.value!!
+    }
+
+    @AssistedInject.Factory
+    interface Factory : AssistedSavedStateViewModelFactory<SignupViewModel>
+}
