59946d7a0cb91f5a8d9c420b84f2bb3a0ff4bebe
jjeewon <wldnjs594@gmail.com>
2021-10-27 21:34:25 +0900

    뷰모델 모듈에 회원가입 뷰모델 추가

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/di/ViewModelModule.kt
@@ -0,0 +1,34 @@
+package com.gomdolstudio.covidinfoapp.di
+
+import androidx.lifecycle.ViewModel
+import com.gomdolstudio.covidinfoapp.di.factory.AssistedSavedStateViewModelFactory
+import com.gomdolstudio.sellers.ui.SignupViewModel
+import com.squareup.inject.assisted.AssistedInject
+import com.squareup.inject.assisted.dagger2.AssistedModule
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import dagger.multibindings.Multibinds
+
+@AssistedModule
+@Module(includes = [AssistedInject_ViewModelModule::class])
+abstract class ViewModelModule {
+
+    // 일반 뷰모델들의 멀티 바인딩
+    @Multibinds
+    abstract fun bindsViewModels(): Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>
+
+    // AssistedInject로 관리하는 ViewModel Factory 멀티 바인딩
+    @Multibinds
+    abstract fun bindAssistedViewModels(): Map<Class<out ViewModel>, @JvmSuppressWildcards AssistedSavedStateViewModelFactory<out ViewModel>>
+
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SignupViewModel::class)
+    abstract fun bindsCasesViewModel(factory: SignupViewModel.Factory): AssistedSavedStateViewModelFactory<out ViewModel>
+
+
+
+
+}
