a46392b8a70de845f5e13f7d02f552eb895272d8
jjeewon <wldnjs594@gmail.com>
2021-10-27 22:06:14 +0900

    회원가입 액티비티 정의

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/SignupActivity.kt
@@ -0,0 +1,29 @@
+package com.gomdolstudio.sellers.ui
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.gomdolstudio.sellers.databinding.ActivitySignupBinding
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+class SignupActivity : DaggerAppCompatActivity() {
+    @Inject
+    lateinit var binding: ActivitySignupBinding
+    @Inject
+    lateinit var viewModelProvider: ViewModelProvider
+    private lateinit var signupViewModel: SignupViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        signupViewModel = viewModelProvider.get(SignupViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.signupViewModel = signupViewModel
+
+        signupViewModel.getShowToast().observe(this,
+            Observer {text -> Toast.makeText(this, text, Toast.LENGTH_LONG).show()})
+
+
+    }
+}
