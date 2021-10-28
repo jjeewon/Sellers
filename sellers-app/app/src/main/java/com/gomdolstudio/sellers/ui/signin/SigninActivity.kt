4bf92839f9674274fb86d284a8ba96e20a206e23
jjeewon <wldnjs594@gmail.com>
2021-10-28 14:20:41 +0900

    레이아웃에 바인딩한 뷰모델이랑 액티비티 뷰모델 연결, SingleLiveEvent로 정의한 String값 구독

--- sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/signin/SigninActivity.kt
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/signin/SigninActivity.kt
@@ -2,11 +2,13 @@ package com.gomdolstudio.sellers.ui.signin
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gomdolstudio.sellers.databinding.ActivitySigninBinding
 import com.gomdolstudio.sellers.databinding.ActivitySignupBinding
 import com.gomdolstudio.sellers.ui.signup.SignupViewModel
 import dagger.android.support.DaggerAppCompatActivity
+
 import javax.inject.Inject
 
 class SigninActivity : DaggerAppCompatActivity() {
@@ -20,7 +22,10 @@ class SigninActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         signinViewModel = viewModelProvider.get(SigninViewModel::class.java)
         binding.lifecycleOwner = this
-        binding.signupViewModel = signinViewModel
+        binding.signinViewModel = signinViewModel
+
+        signinViewModel.getShowToast().observe(this,
+                Observer {text -> Toast.makeText(this, text, Toast.LENGTH_LONG).show()})
 
     }
 }
