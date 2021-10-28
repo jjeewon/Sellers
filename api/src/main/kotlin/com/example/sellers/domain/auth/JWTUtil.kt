c49ad67eedc4a2eb607443808621bc457bb49c17
jjeewon <wldnjs594@gmail.com>
2021-10-28 09:28:02 +0900

    리프레쉬 토큰 만드는 함수 정의
    API용 토큰 : 몇시간 단위(여기선 2시간)의 짧은 만료시간을 가짐
    리프레쉬 토큰 : API용 토큰이 만료되었을 때 리프레쉬 토큰을 이용해 새로운 API 토큰을 발급받음. 리프레쉬토큰은 만료되었을 때 로그인 다시해야하므로 만료시간을 길게(여기선 5시간) 설정함

--- api/src/main/kotlin/com/example/sellers/domain/auth/JWTUtil.kt
+++ api/src/main/kotlin/com/example/sellers/domain/auth/JWTUtil.kt
@@ -9,8 +9,12 @@ object JWTUtil {
     private const val ISSUER = "Sellers"
     private const val SUBJECT = "Auth"
     private const val EXPIRE_TIME = 60L * 60 * 2 * 1000 // 2시간
+    private const val REFRESH_EXPIRE_TIME = 60L * 60 * 24 * 30 * 1000 // 30일
     private val SECRET = "your-secret"
     private val algorithm: Algorithm = Algorithm.HMAC256(SECRET)
+
+    private val refreshSecret = "your-refresh-secret"
+    private val refreshAlgorithm: Algorithm = Algorithm.HMAC256(refreshSecret)
 
     fun createToken(email: String) = JWT.create()
         .withIssuer(ISSUER)
@@ -19,6 +23,14 @@ object JWTUtil {
         .withExpiresAt(Date(Date().time + EXPIRE_TIME))
         .withClaim(JWTClaims.EMAIL, email)
         .sign(algorithm)
+
+    fun createRefreshToken(email: String) = JWT.create()
+        .withIssuer(ISSUER)
+        .withSubject(SUBJECT)
+        .withIssuedAt(Date())
+        .withExpiresAt(Date(Date().time + REFRESH_EXPIRE_TIME))
+        .withClaim(JWTClaims.EMAIL, email)
+        .sign(refreshAlgorithm)
 
     object JWTClaims{
         const val EMAIL = "email"
