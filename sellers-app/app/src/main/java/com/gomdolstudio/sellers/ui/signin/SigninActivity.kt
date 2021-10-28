654f8f3c38253578dd8655bd2a38011a9925ca27
jjeewon <wldnjs594@gmail.com>
2021-10-29 00:17:10 +0900

    클릭이벤트 구독하다가 변화사항 생기면 액티비티 전환

--- sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/signin/SigninActivity.kt
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/signin/SigninActivity.kt
@@ -1,11 +1,14 @@
 package com.gomdolstudio.sellers.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gomdolstudio.sellers.databinding.ActivitySigninBinding
 import com.gomdolstudio.sellers.databinding.ActivitySignupBinding
+import com.gomdolstudio.sellers.ui.product.ProductMainActivity
+import com.gomdolstudio.sellers.ui.signup.SignupActivity
 import com.gomdolstudio.sellers.ui.signup.SignupViewModel
 import dagger.android.support.DaggerAppCompatActivity
 
@@ -26,6 +29,14 @@ class SigninActivity : DaggerAppCompatActivity() {
 
         signinViewModel.getShowToast().observe(this,
                 Observer {text -> Toast.makeText(this, text, Toast.LENGTH_LONG).show()})
+        signinViewModel.getSignupClickEvent().observe(this,
+                Observer {
+                    startActivity(Intent(this@SigninActivity, SignupActivity::class.java))
+                    finish()})
+        signinViewModel.getMoveToProductMainEvent().observe(this,
+            Observer {
+                startActivity(Intent(this@SigninActivity, ProductMainActivity::class.java))
+                finish()})
 
     }
 }
