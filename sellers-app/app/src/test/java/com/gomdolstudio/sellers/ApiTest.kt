516cac27e24d8433a3f087ddba2914f2a71be48f
jjeewon <wldnjs594@gmail.com>
2021-10-28 10:38:42 +0900

    회원가입 API 테스트 코드 작성

--- sellers-app/app/src/test/java/com/gomdolstudio/sellers/ApiTest.kt
+++ sellers-app/app/src/test/java/com/gomdolstudio/sellers/ApiTest.kt
@@ -1,6 +1,8 @@
 package com.gomdolstudio.sellers
 
+import com.gomdolstudio.sellers.api.request.SignupRequest
 import com.gomdolstudio.sellers.data.service.HelloService
+import com.gomdolstudio.sellers.data.service.SignupService
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import org.junit.Before
 import org.junit.Test
@@ -9,6 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiTest {
     lateinit var helloService: HelloService
+    lateinit var signupService: SignupService
 
     @Before
     fun 데이터서비스세팅(){
@@ -18,10 +21,23 @@ class ApiTest {
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
             .create(HelloService::class.java)
+
+        signupService = Retrofit.Builder()
+                .baseUrl("http://localhost:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(SignupService::class.java)
     }
 
     @Test
     fun 헬로데이터테스트(){
         helloService.getWorld().subscribe(System.out::println)
+    }
+
+    @Test
+    fun 회원가입테스트(){
+        val request: SignupRequest  = SignupRequest("seller@naver.com","sellerssellers","seller")
+        signupService.signup(request).subscribe(System.out::println)
     }
 }
