934659ca51fd91d5243fc66eb94ee38671630b5c
jjeewon <wldnjs594@gmail.com>
2021-10-28 11:45:51 +0900

    로그인 액티비티 정의
    [의존성 주입]
    - 로그인레이아웃바인딩
    - 뷰모델프로바이더
    
    바인딩에 로그인뷰모델 연결

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/ui/signin/SigninActivity.kt
@@ -0,0 +1,26 @@
+package com.gomdolstudio.sellers.ui.signin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.gomdolstudio.sellers.databinding.ActivitySigninBinding
+import com.gomdolstudio.sellers.databinding.ActivitySignupBinding
+import com.gomdolstudio.sellers.ui.signup.SignupViewModel
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+class SigninActivity : DaggerAppCompatActivity() {
+    @Inject
+    lateinit var binding: ActivitySigninBinding
+    @Inject
+    lateinit var viewModelProvider: ViewModelProvider
+    private lateinit var signinViewModel: SigninViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        signinViewModel = viewModelProvider.get(SigninViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.signupViewModel = signinViewModel
+
+    }
+}
