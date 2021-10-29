5192be21587ee6ce011b7dddca94816b7a9c2e97
jjeewon <wldnjs594@gmail.com>
2021-10-29 12:15:55 +0900

    토큰 검증에 필요한 상수 , 오류 로깅을 위한 로거 변수 정의
    AUTHORIZATION : Authorization 토큰이 포함된 헤더값 가져오기 위한 상수
    BEARER : Authorization 헤더에 JWT 토큰 전달할 때 사용되는 인증 방법을 나타낸 스키마. 실제 토큰 사용하려면 헤더 값에서 이 문자열 제거한 다음에 사용해야 함
    GRANT_TYPE, GRANT_TYPE_REFRESH : 토큰 재발행 요총할 때 사용될 파라미터의 키, 값
    DEFAULT_ALLOWED_API_URLS : 토큰 인정없이 사용가능한 URL 정의한 리스트

--- api/src/main/kotlin/com/example/sellers/interceptor/TokenValidationInterceptor.kt
+++ api/src/main/kotlin/com/example/sellers/interceptor/TokenValidationInterceptor.kt
@@ -11,4 +11,15 @@ class TokenValidationInterceptor @Autowired constructor(
     private val userContextHolder: UserContextHolder
 ) : HandlerInterceptor {
     private val logger = LoggerFactory.getLogger(this::class.java)
+    companion object {
+        private const val AUTHORIZATION = "Authorization"
+        private const val BEARER = "Bearer"
+        private const val GRANT_TYPE = "grant_type"
+        const val GRANT_TYPE_REFRESH = "refresh_token"
+
+        private val DEFAULT_ALLOWED_API_URLS = listOf(
+            "POST" to "/api/v1/signin",
+            "POST" to "/api/v1/users"
+        )
+    }
 }
