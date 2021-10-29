0770f1f10caa4f398d7b5331f6d2bcbb1a3428c8
jjeewon <wldnjs594@gmail.com>
2021-10-29 12:05:49 +0900

    ThreadLocal 변수를 가지는 UserContextHolder 클래스 정의

---
+++ api/src/main/kotlin/com/example/sellers/domain/auth/UserContextHolder.kt
@@ -0,0 +1,36 @@
+package com.example.sellers.domain.auth
+
+import com.example.sellers.domain.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class UserContextHolder @Autowired constructor(
+    private val userRepository: UserRepository
+) {
+    private val userHolder = ThreadLocal.withInitial{
+        UserHolder()
+    }
+    val id: Long? get() = userHolder.get().id
+    val name: String? get() = userHolder.get().name
+    val email: String? get() = userHolder.get().email
+
+    fun set(email:String) = userRepository
+        .findByEmail(email)?.let { user ->
+            this.userHolder.get().apply {
+                this.id = user.id
+                this.name = user.name
+                this.email = user.email
+            }.run { userHolder::set }
+        }
+
+    fun clear(){
+        userHolder.remove()
+    }
+
+    class UserHolder{
+        var id: Long? = null
+        var email: String? = null
+        var name: String? = null
+    }
+}
