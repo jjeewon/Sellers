a65d57358823d0ffdb5af60e09a2a6e7975cd980
jjeewon <wldnjs594@gmail.com>
2021-10-29 15:19:27 +0900

    postHandle - 사용자 정보 초기화

--- api/src/main/kotlin/com/example/sellers/interceptor/TokenValidationInterceptor.kt
+++ api/src/main/kotlin/com/example/sellers/interceptor/TokenValidationInterceptor.kt
@@ -1,10 +1,12 @@
 package com.example.sellers.interceptor
 
+import com.example.sellers.domain.auth.JWTUtil
 import com.example.sellers.domain.auth.UserContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -15,9 +17,52 @@ class TokenValidationInterceptor @Autowired constructor(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        return super.preHandle(request, response, handler)
+        val authHeader = request.getHeader(AUTHORIZATION)
+        if(authHeader.isNullOrBlank()) {
+            val pair = request.method to request.servletPath
+            if (!DEFAULT_ALLOWED_API_URLS.contains(pair)) {
+                response.sendError(401)
+                return false
+            }
+            return true
+        } else {
+            val grantType = request.getParameter("grant_type")
+            val token = extractToken(authHeader)
+            return handleToken(grantType, token, response)
+        }
     }
-    
+
+    private fun handleToken(
+        grantType: String,
+        token: String,
+        response: HttpServletResponse
+    ) = try {
+        val jwt = when(grantType) {
+            GRANT_TYPE_REFRESH -> JWTUtil.verifyRefresh(token)
+            else -> JWTUtil.verify(token)
+        }
+        val email = JWTUtil.extractEmail(jwt)
+        userContextHolder.set(email)
+        true
+    } catch (e: Exception){
+        logger.error("토큰 검증 실패. token = $token", e)
+        response.sendError(401)
+        false
+    }
+
+    private fun extractToken(token: String) =
+        token.replace(BEARER, "").trim()
+
+
+    override fun postHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        modelAndView: ModelAndView?
+    ) {
+        userContextHolder.clear()
+    }
+
     companion object {
         private const val AUTHORIZATION = "Authorization"
         private const val BEARER = "Bearer"
