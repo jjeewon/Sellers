96991441b19bb888cd629c5030ced7ae04f90570
jjeewon <wldnjs594@gmail.com>
2021-10-29 18:42:24 +0900

    상품저장 Request 클래스 정의
    기본정보 + 미리 등록된 이미지들의 id를 리스트로 받기 위해 Long 타입의 리스트로 프로퍼티 추가

---
+++ api/src/main/kotlin/com/example/sellers/domain/product/registration/ProductRegistrationRequest.kt
@@ -0,0 +1,9 @@
+package com.example.sellers.domain.product.registration
+
+data class ProductRegistrationRequest (
+    val name: String,
+    val description: String,
+    val price: Int,
+    val categoryId: Int,
+    val imagesIds: List<Long?>
+        )
