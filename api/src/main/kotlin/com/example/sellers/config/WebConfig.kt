53d3d931dcde70d94cdb5fd28d9e1131670515b5
jjeewon <wldnjs594@gmail.com>
2021-10-29 15:32:19 +0900

    TokenValidationInterceptor 등록
    @Configuration : 스프링에서 사용하는 설정을 담은 빈 클래스
    >> SpringBootApplication 이하 패키지의 모든 설정 클래스를 검사하여 자동으로 빈 생성함

---
+++ api/src/main/kotlin/com/example/sellers/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.example.sellers.config
+
+import com.example.sellers.interceptor.TokenValidationInterceptor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig @Autowired constructor(
+    private val tokenValidationInterceptor: TokenValidationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(tokenValidationInterceptor)
+            .addPathPatterns("/api/**")
+    }
+}
