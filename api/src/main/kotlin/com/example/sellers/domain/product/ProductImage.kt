6d9a7ae8aa6bf324bfa41fde5887ca61883c73d2
jjeewon <wldnjs594@gmail.com>
2021-10-29 09:20:14 +0900

    상품 이미지 엔티티 정의

---
+++ api/src/main/kotlin/com/example/sellers/domain/product/ProductImage.kt
@@ -0,0 +1,29 @@
+package com.example.sellers.domain.product
+
+import java.util.*
+import javax.persistence.*
+
+@Entity(name = "product_image")
+class ProductImage (
+    var path: String,
+    var productId: Long? = null
+){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    var createdAt: Date? = null
+    var updatedAt: Date? = null
+
+    @PrePersist
+    fun prePersist(){
+        createdAt = Date()
+        updatedAt = Date()
+    }
+
+    @PreUpdate
+    fun preUpdate(){
+        updatedAt = Date()
+    }
+}
+
