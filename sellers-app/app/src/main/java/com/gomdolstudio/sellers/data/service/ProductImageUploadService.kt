81b76f827f35e20f66ce518b35f497d83e0e820d
jjeewon <wldnjs594@gmail.com>
2021-10-30 22:23:37 +0900

    이미지업로드 서비스 정의

---
+++ sellers-app/app/src/main/java/com/gomdolstudio/sellers/data/service/ProductImageUploadService.kt
@@ -0,0 +1,15 @@
+package com.gomdolstudio.sellers.data.service
+
+import com.gomdolstudio.sellers.api.response.ApiResponse
+import com.gomdolstudio.sellers.api.response.ProductImageUploadResponse
+import io.reactivex.Single
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ProductImageUploadService {
+    @Multipart
+    @POST("/api/v1/product_images")
+    fun uploadProductImages(@Part images: MultipartBody.Part) : Single<ApiResponse<ProductImageUploadResponse>>
+}
